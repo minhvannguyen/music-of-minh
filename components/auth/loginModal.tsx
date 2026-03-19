8bec02520c759b88f9a6922939c06bc9e3100a30
minhvannghuyen <minhvannghuyen@gmail.com>
2026-03-19 22:40:48 +0700

    fix center login google

--- components/auth/loginModal.tsx
+++ components/auth/loginModal.tsx
@@ -266,7 +266,7 @@ export default function LoginModal({
                 buttonReady && !isLoadingGoogleAuth ? "w-full" : "hidden"
               }
               style={{
-                width: 225,
+                width: 220,
                 margin: "0 auto",
                 display: buttonReady && !isLoadingGoogleAuth ? "block" : "none",
                 opacity: isLoadingGoogleAuth ? 0.5 : 1,
